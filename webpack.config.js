32e811f6d1c663d3d6ffb875d2a92750d9dc6ade
Yashawant Retharekar <yashwant.retharekar@gmail.com>
2017-12-15 01:40:44 +0530

    setup_initial_environment

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: '"./src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: ['react-hot-loader/webpack', 'babel-loader']
+            }
+        ]
+    },
+    stats: {
+        colors: true
+    },
+    devServer : {
+        inline: true,
+        contentBase: './dist',
+        port: 8100,
+        historyApiFallback: true
+    }
+};
