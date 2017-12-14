9d80ce7bb522cc162817f663d3da6d32674c50d1
Yashawant Retharekar <yashwant.retharekar@gmail.com>
2017-12-15 02:12:16 +0530

    working on front end

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+    render(){
+        return(
+            <div>
+                <h4>
+                    Hello World!!
+                </h4>    
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
